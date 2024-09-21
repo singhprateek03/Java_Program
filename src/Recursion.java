7e2bcf633be102501cd8ab7b0dac5bf95d4d76d9
Prateek Singh <prateeksingh3102000@gmail.com>
2024-09-21 13:23:48 +0530

    Update Recursion.java

--- src/Recursion.java
+++ src/Recursion.java
@@ -12,7 +12,8 @@ public class Recursion {
     static int factorial(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        } else {
+        }
+        else {
         return n * factorial(n-1);
         }
     }
